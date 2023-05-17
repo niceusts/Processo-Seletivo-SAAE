100fdfe61c9a73a4e19f884bfc0e0e7670b7f5e0
niceu <niceusantos1365@gmail.com>
2023-05-17 10:53:20 -0300

    First Commit

---
+++ Processo-Seletivo-SAAE/Controllers/Form.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Processo_Seletivo_SAAE.Controllers
+{
+    public class Form : Controller
+    {
+        public IActionResult FormFiles()
+        {
+            return View();
+        }
+        public IActionResult InfoFiles()
+        {
+            return View();
+        }
+    }
+}
