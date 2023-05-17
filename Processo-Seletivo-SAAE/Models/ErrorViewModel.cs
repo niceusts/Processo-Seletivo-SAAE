f726acc7bbecf5d863f7c34f3adbd3bb3cb204cb
niceu <niceusantos1365@gmail.com>
2023-05-17 10:11:38 -0300

    Adicionar arquivos de projeto.

---
+++ Processo-Seletivo-SAAE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Processo_Seletivo_SAAE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
